634431c0c75e221b84999ecf0c79f1b8e2d89ced
craisin <kahkaicyap@gmail.com>
2026-04-07 14:16:30 -0700

    design doc update

--- client.ino
+++ client.ino
@@ -1,7 +1,7 @@
-#include "BLEDevice.h"
+//#include "BLEDevice.h"
 
 static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-static BLEUUID    charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+static BLEUUID charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
 
 static boolean doConnect = false;
 static boolean connected = false;
