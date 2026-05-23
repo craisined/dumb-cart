9ed5225d3f516e36fb20ce5c76d81bf63ac61e50
craisin <kahkaicyap@gmail.com>
2026-05-22 17:09:57 -0700

    halfway program basic detection functions

--- client/client.ino
+++ client/client.ino
@@ -1,12 +1,50 @@
-#include <BLEDevice.h>
+#include "LSM6DS3.h"
+#include "Wire.h"
+#include "Encoder.h"
+#include "Adafruit_HX711.h"
 
-const String serivce_uuid = "ae936f5c-108b-4eb4-a5f6-fc979626e073";
-const String characteristic_uuid = "5d43ca63-8167-45a9-b697-e28945b6399c";
+const float gravity = 9.81;
+const int tick_circumference = 1;
+
+const accelerometer_addr = 0x6A;
+LSM6DS3 accelerometer(I2C_MODE, accelerometer_addr);
+
+// Pins not accurate yet
+const uint8_t load_cell_data_pin = 0;
+const uint8_t load_cell_clock_pin = 1;
+Adafruit_HX711 load_cell(load_cell_data_pin, load_cell_clock_pin);
+
+
+// Pins not accurate yet
+const uint8_t encoder_pin_1 = 0;
+const uint8_t encoder_pin_2 = 1;
+Encoder encoder(pin1, pin2);
 
 void setup() {
     Serial.begin(9600);
+    accelerometer.begin();
+    load_cell.begin();
+}
+
+float get_x_acceleration(){
+    return accelerometer.readFloatAccelX() * gravity;
+}
+
+float get_force(){
+    return 0.1;
+}
+
+float get_encoder_pos(){
+    return myEnc.read();
 }
 
 void loop() {
-    Serial.println("Hello world");
+    Serial.print(get_x_acceleration());
+    Serial.println(" m/s");
+
+    Serial.print(get_force());
+    Serial.println(" N");
+
+    Serial.print(get_encoder_pos());
+    Serial.println(" ticks");
 }
