5015cc16fea3ad26d1fd601beb872d5cce4015a9
craisin <kahkaicyap@gmail.com>
2026-05-22 17:31:59 -0700

    put sensors in a class

--- client/client.ino
+++ client/client.ino
@@ -6,45 +6,49 @@
 const float gravity = 9.81;
 const int tick_circumference = 1;
 
-const accelerometer_addr = 0x6A;
-LSM6DS3 accelerometer(I2C_MODE, accelerometer_addr);
+class Sensors{
 
-// Pins not accurate yet
-const uint8_t load_cell_data_pin = 0;
-const uint8_t load_cell_clock_pin = 1;
-Adafruit_HX711 load_cell(load_cell_data_pin, load_cell_clock_pin);
+    //Pins not updated
+    const uint8_t accelerometer_addr = 0x6A;
+    const uint8_t load_cell_data_pin = 0;
+    const uint8_t load_cell_clock_pin = 1;
+    const uint8_t encoder_pin_1 = 0;
+    const uint8_t encoder_pin_2 = 1;
 
+    LSM6DS3 accelerometer(I2C_MODE, accelerometer_addr);
+    Adafruit_HX711 load_cell(load_cell_data_pin, load_cell_clock_pin);
+    Encoder encoder(encoder_pin_1, encoder_pin_2);
 
-// Pins not accurate yet
-const uint8_t encoder_pin_1 = 0;
-const uint8_t encoder_pin_2 = 1;
-Encoder encoder(pin1, pin2);
+public:
+    void begin(){
+        this.accelerometer.begin();
+        this.load_cell.begin();
+        for (uint8_t t=0; t<3; t++) {
+            this.load_cell.tareA(this.load_cell.readChannelRaw(CHAN_A_GAIN_128));
+        }
+    }
+
+    float get_acceleration(){
+        return this.accelerometer.readFloatAccelX() * gravity;
+    }
+
+    float get_force(){
+        return this.load_cell.readChannelBlocking(CHAN_A_GAIN_128);
+    }
+
+    float get_encoder_pos(){
+        return this.encoder.read();
+    }
+
+};
+
+Sensors sensors();
 
 void setup() {
     Serial.begin(9600);
-    accelerometer.begin();
-    load_cell.begin();
-}
+    sensors.begin();
 
-float get_x_acceleration(){
-    return accelerometer.readFloatAccelX() * gravity;
-}
-
-float get_force(){
-    return 0.1;
-}
-
-float get_encoder_pos(){
-    return myEnc.read();
 }
 
 void loop() {
-    Serial.print(get_x_acceleration());
-    Serial.println(" m/s");
-
-    Serial.print(get_force());
-    Serial.println(" N");
-
-    Serial.print(get_encoder_pos());
-    Serial.println(" ticks");
 }
