f8fc475480ed77e7bcf8110eb13091c2500f64f1
craisin <kahkaicyap@gmail.com>
2026-05-24 22:19:36 -0700

    prelim bluetooth test

--- client/client.ino
+++ client/client.ino
@@ -2,6 +2,9 @@
 #include <Wire.h>
 #include <Encoder.h>
 #include <Adafruit_HX711.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 const float gravity = 9.81;
 const int tick_circumference = 1;
@@ -16,6 +19,9 @@ const uint8_t encoder_pin_2 = D6;
 LSM6DS3 accelerometer(I2C_MODE, accelerometer_addr);
 Adafruit_HX711 load_cell(load_cell_data_pin, load_cell_clock_pin);
 Encoder encoder(encoder_pin_1, encoder_pin_2);
+
+const String service_uuid = "907da526-6f31-42c6-8b17-4fa0c76ad1d7";
+const String characteristic_uuid = "b05f71fc-c06c-47b0-ad5c-623db0469d4d";
 
 void begin_sensors(){
     accelerometer.begin();
@@ -37,10 +43,27 @@ float get_encoder_pos(){
   return encoder.read();
 }
 
+void begin_bluetooth(){
+    BLEDevice::init("Dumb Cart");
+    BLEServer *pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(service_uuid);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+        characteristic_uuid,
+        BLECharacteristic::PROPERTY_READ
+    );
+    pCharacteristic->setValue("Hello World");
+    pService->start();
+
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(service_uuid); 
+    pAdvertising->setScanResponse(true);
+    BLEDevice::startAdvertising();
+}
 
 void setup() {
     Serial.begin(9600);
     begin_sensors();
+    begin_bluetooth();
 }
 
 void loop() {
