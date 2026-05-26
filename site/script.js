9caf52482b2a31398d98beaa4442e3fd6d2dd256
craisin <kahkaicyap@gmail.com>
2026-05-26 11:15:03 -0700

    bluetooth

---
+++ site/script.js
@@ -0,0 +1,48 @@
+const service_uuid = "907da526-6f31-42c6-8b17-4fa0c76ad1d7";
+const characteristic_uuid = "383dfe4a-06d1-49bc-862f-06841d591a7e";
+
+const connect_btn = document.getElementById("connect");
+
+let ble_server;
+let ble_service;
+let ble_sensor_characteristic;
+
+function web_bluetooth_enabled(){
+    if (navigator.bluetooth){
+        console.log("Bluetooth enabled!");
+        return true;
+    }
+    console.log("Bluetooth disabled");
+    return false;
+}
+
+function handle_characteristic_change(event){
+    let buffer = event.target.value.buffer;
+    let dataview = new DataView(buffer);
+    let offset = 0;
+    let sensors = {
+        acceleration: dataview.getFloat32(offset, true),
+        force: dataview.getFloat32(offset += 4, true),
+        encoder: dataview.getFloat32(offset += 4, true),
+        time: dataview.getInt32(offset += 4, true)
+    };
+    console.log(sensors);
+}
+
+function connect(event){
+    if (!web_bluetooth_enabled()){
+        return null;
+    }
+    navigator.bluetooth.requestDevice({
+        optionalServices: [service_uuid]
+    })
+    .then(device => {
+        console.log("Device: ", device.name);
+        ble_server = device.gatt.connect();
+        ble_service = ble_server.getPrimaryService(service_uuid);
+        ble_sensor_characteristic = ble_service.getCharacteristic(characteristic_uuid);
+        ble_sensor_characteristic.startNotifications();
+        ble_sensor_characteristic.addEventListener('characteristicvaluechanged', handle_characteristic_change);
+        console.log("Connected!");
+    })
+}
