82618cefcb41636457921d6dbd6273d5524d162f
craisin <kahkaicyap@gmail.com>
2026-05-27 11:08:49 -0700

    move out ble

---
+++ site/ble/bluetooth.js
@@ -0,0 +1,58 @@
+const service_uuid = "907da526-6f31-42c6-8b17-4fa0c76ad1d7";
+const characteristic_uuid = "383dfe4a-06d1-49bc-862f-06841d591a7e";
+
+const connect_btn = document.getElementById("connect");
+const disconnect_btn = document.getElementById("disconnect");
+
+let ble_server;
+let ble_service;
+let ble_sensor_characteristic;
+
+function web_bluetooth_enabled(){
+    if (navigator.bluetooth){
+        console.log("Bluetooth enabled!");
+        return true;
+    }
+    console.log("Bluetooth disabled");
+    return false;
+}
+
+function handle_characteristic_change(event){
+    let buffer = event.target.value.buffer;
+    let dataview = new DataView(buffer);
+    let offset = 0;
+    let sensors = {
+        acceleration: dataview.getFloat32(offset, true),
+        force: dataview.getFloat32(offset += 4, true),
+        encoder: dataview.getFloat32(offset += 4, true),
+        time: dataview.getInt32(offset += 4, true)
+    };
+    console.log(sensors);
+}
+
+async function connect(event){
+    if (!web_bluetooth_enabled()){
+        return null;
+    }
+    const device = await navigator.bluetooth.requestDevice({
+        filters: [{ services: [service_uuid] }],
+    })
+    console.log("Device: ", device.name);
+    const ble_server = await device.gatt.connect();
+    const ble_service = await ble_server.getPrimaryService(service_uuid);
+    const ble_sensor_characteristic = await ble_service.getCharacteristic(characteristic_uuid);
+    ble_sensor_characteristic.startNotifications();
+    ble_sensor_characteristic.addEventListener('characteristicvaluechanged', handle_characteristic_change);
+    console.log("Connected!");
+}
+
+async function disconnect(event){
+    if (ble_server && ble_server.connected){
+        return null;
+    }
+    ble_sensor_characteristic.stopNotifications();
+    ble_server.disconnect();
+}
+
+connect_btn.addEventListener("click", connect);
+disconnect_btn.addEventListener("click", disconnect);
